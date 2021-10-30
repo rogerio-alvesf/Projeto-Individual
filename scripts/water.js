c09d91a2b75c691629eb62276afba5cf20d7ae86
rogerio-alvesf <rogerio.filho@bandtec.com.br>
2021-10-30 17:42:20 -0300

    Finalização da calculara do arquivo water, finalização do arquivo water e criação do footer para os arquivos: water e home

--- scripts/water.js
+++ scripts/water.js
@@ -1,23 +1,43 @@
-function calculate(){
+function calculate() {
     var years_old = Number(in_years_old.value);
     var weight = Number(in_weight.value);
-    
-    if(years_old == "" && weight == ""){
-        window.alert("Preencha todos os campos");
-    }else if(years_old == ""){
-        window.alert("Preencha o campo idade");
-    }else if(weight == ""){
-        window.alert("Preencha o campo peso");
-    }else if(years_old < 0 && weight < 0){
-        window.alert("Idade e peso invalidos");
-    }else if(years_old < 0){
-        window.alert("Idade invalida");
-    }else if(weight < 0){
-        window.alert("Pesso invalido");
-    }else if(years_old <= 17){
-        var ml_per_kg = 40;
-        var ml_water_per_day = ml_per_kg * weight;
-        var litro_water_per_day = ml_water_per_day / 1000;
-        p_result.innerHTML = `Você deve beber<b>${ml_water_per_day} ml</b> ou <b>${litro_water_per_day} litros</b>de água por dia.`;
+
+    if (years_old == "" || weight == "") {
+        if (years_old == "" && weight == "") {
+            window.alert("Preencha todos os campos");
+        } else if (years_old == "") {
+            window.alert("Preencha o campo idade");
+        } else if (weight == "") {
+            window.alert("Preencha o campo peso");
+        } else if (years_old < 0 && weight < 0) {
+            window.alert("Idade e peso invalidos");
+        } else if (years_old < 0) {
+            window.alert("Idade invalida");
+        } else if (weight < 0) {
+            window.alert("Pesso invalido");
+        }
+    } else if (years_old != "" && weight != ""){
+        if (years_old <= 17) {
+            var ml_per_kg = 40;
+            var ml_water_per_day = ml_per_kg * weight;
+            var litro_water_per_day = ml_water_per_day / 1000;
+            p_result.innerHTML = `O ideal é você tomar <b>${ml_water_per_day} ml</b> ou <b>${litro_water_per_day} litros</b> de água por dia.`;
+        } else if (years_old == 18 || years_old <= 55) {
+            var ml_per_kg = 35;
+            var ml_water_per_day = ml_per_kg * weight;
+            var litro_water_per_day = ml_water_per_day / 1000;
+            p_result.innerHTML = `O ideal é você tomar <b>${ml_water_per_day} ml</b> ou <b>${litro_water_per_day} litros</b> de água por dia.`;
+        } else if (years_old == 55 || years_old <= 65) {
+            var ml_per_kg = 30;
+            var ml_water_per_day = ml_per_kg * weight;
+            var litro_water_per_day = ml_water_per_day / 1000;
+            p_result.innerHTML = `O ideal é você tomar <b>${ml_water_per_day} ml</b> ou <b>${litro_water_per_day} litros</b> de água por dia.`;
+        } else if (years_old > 65) {
+            var ml_per_kg = 25;
+            var ml_water_per_day = ml_per_kg * weight;
+            var litro_water_per_day = ml_water_per_day / 1000;
+            p_result.innerHTML = `O ideal é você tomar <b>${ml_water_per_day} ml</b> ou <b>${litro_water_per_day} litros</b> de água por dia.`;
+        }
+    };
+        p_result.style.display = "block";
     }
-}
